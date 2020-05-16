91a7250c1054746158dc020f936a64f202262973
Gui Seek <guiseek@gmail.com>
2020-05-16 00:06:29 -0300

    Front
    - Adicionado @angular/pwa
    - Adicionado @angular/fire
    - SVG Covid 19

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '@env/environment';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
